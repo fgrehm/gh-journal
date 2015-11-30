1063bd40f98c5b40c0500129501667b1f400ef82
Fabio Rehm <fgrehm@gmail.com>
2015-11-29 22:51:02 -0200

    Refactor a bit more

--- src/cmd/gh-journal/main.go
+++ src/cmd/gh-journal/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"ghjournal"
 	"gopkg.in/mgo.v2"
-	"log"
 	"os"
 )
 
@@ -23,32 +22,7 @@ func main() {
 	defer session.Close()
 
 	repo := ghjournal.NewEventsRepository(session.DB("gh-journal"))
-	for pageNum := 1; pageNum <= 10; pageNum++ {
-		log.Printf("Importing events from page %d...", pageNum)
-		ghEvents, err := client.Events(pageNum)
-		if err != nil {
-			log.Fatal(err)
-		}
-		createdAtLeastOneEvent := false
-		for _, event := range ghEvents {
-			id := event["id"].(string)
-			if exists, err := repo.Exists(id); err != nil {
-				log.Fatal(err)
-			} else if exists {
-				log.Printf("SKIP ID=%s", id)
-				continue
-			}
-
-			createdAtLeastOneEvent = true
-			log.Printf("CREATE ID=%s", id)
-			err = repo.Insert(event)
-			if err != nil {
-				log.Fatal(err)
-			}
-		}
-		if !createdAtLeastOneEvent {
-			log.Printf("Done with import at page %d", pageNum)
-			break
-		}
+	if err = ghjournal.ImportEvents(client, repo); err != nil {
+		panic(err)
 	}
 }
---
+++ src/ghjournal/gh_events_importer.go
@@ -0,0 +1,37 @@
+package ghjournal
+
+import (
+  "log"
+)
+
+func ImportEvents(client GitHubClient, repo EventsRepository) error {
+  for pageNum := 1; pageNum <= 10; pageNum++ {
+    log.Printf("Importing events from page %d...", pageNum)
+    ghEvents, err := client.Events(pageNum)
+    if err != nil {
+      return err
+    }
+    createdAtLeastOneEvent := false
+    for _, event := range ghEvents {
+      id := event["id"].(string)
+      if exists, err := repo.Exists(id); err != nil {
+        return err
+      } else if exists {
+        log.Printf("SKIP ID=%s", id)
+        continue
+      }
+
+      createdAtLeastOneEvent = true
+      log.Printf("CREATE ID=%s", id)
+      err = repo.Insert(event)
+      if err != nil {
+        return err
+      }
+    }
+    if !createdAtLeastOneEvent {
+      log.Printf("Done with import at page %d", pageNum)
+      break
+    }
+  }
+  return nil
+}
